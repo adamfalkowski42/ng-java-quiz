8b5b74f5a008ce4128f266b1824f4d126ca3a7ba
falkowa42 <falkowa42@gmail.com>
2021-07-15 07:54:51 -0400

    Coded quiz taking scenarios, almost all working except for leaving quiz page and coming back does not update quiz questions not taken correctly

--- src/app/quiz/take-quiz.component.ts
+++ src/app/quiz/take-quiz.component.ts
@@ -4,53 +4,70 @@ import {Question} from "../model/question.model";
 import {QuestionService} from "../services/question.service";
 
 @Component({
-  selector:'app-take-quiz',
-  templateUrl:'take-quiz.component.html',
-  styleUrls:['take-quiz.component.css']
+  selector: 'app-take-quiz',
+  templateUrl: 'take-quiz.component.html',
+  styleUrls: ['take-quiz.component.css']
 })
-export class TakeQuizComponent implements OnInit{
-  private questions: Array<Question>;
+export class TakeQuizComponent implements OnInit {
+  private questions: Array<Question> = new Array<Question>();
+  private questionsAnswered: Array<Question> = new Array<Question>();
+  private questionsNotAnswered: Array<Question> = new Array<Question>();
   public chosenQuestion: Question;
   showAnswer: boolean = false;
-  private questionId: number[] = [];
-  public resetQuestions: boolean =false;
+  private questionId: Array<number> = new Array<number>();
+  public resetQuestions: boolean = false;
 
-  constructor(private route: ActivatedRoute,private questionService: QuestionService) {
+
+  constructor(private route: ActivatedRoute, private questionService: QuestionService) {
   }
+
   ngOnInit() {
     this.questions = this.route.snapshot.data['questions'];
-    this.questions.forEach(question =>{
+    this.questions.forEach(
+      question => {
+        if (question.answered) {
+          this.questionsAnswered.push(question);
+        }
+
+      }
+    )
+    this.questions.forEach(
+      question => {
+        if (!question.answered) {
+          this.questionsNotAnswered.push(question);
+        }
+      }
+    )
+    this.questionsNotAnswered.forEach(question => {
       this.questionId.push(question.id);
     })
   }
 
-  getQuestion(): Question{
-    let randomId = this.questionId[Math.floor(Math.random() * this.questionId.length)];
-    let returnQuestion;
-    if(this.questions.length > 0){
-      this.questions.forEach( question =>{
-        if(question.id === randomId && !question.answered){
-          returnQuestion=question;
-        }
-      })
+  getQuestion(): Question {
+    let randomId = Math.floor(Math.random() * this.questionId.length);
+    let randomQuestion = this.questionsNotAnswered[randomId];
+    if (randomQuestion && !randomQuestion.answered) {
+      return randomQuestion;
     }
-    return returnQuestion;
+    return randomQuestion;
   }
 
   btnShowAnswer(question: Question) {
-    this.showAnswer=true;
+    this.showAnswer = true;
     this.questionService.updateQuestion(question);
+    this.questionsAnswered.push(question);
+    this.questionsNotAnswered.splice(this.questionsNotAnswered.indexOf(question), 1);
+
   }
 
   nextQuestion() {
-    this.showAnswer=false;
+    this.showAnswer = false;
     let question = this.getQuestion();
-    console.log('this is question:',question);
-    if(question !== undefined){
-      this.chosenQuestion=question;
-    }else{
+    if (question !== undefined && question !== null) {
+      this.chosenQuestion = question;
+    } else {
       this.resetQuestions = true;
-      this.chosenQuestion=null;
+      this.chosenQuestion = null;
     }
   }
 
@@ -59,14 +76,23 @@ export class TakeQuizComponent implements OnInit{
   }
 
   resetQuiz() {
-    this.questions.forEach(question=>{
+    this.questionsAnswered.forEach(question => {
       question.answered = false;
     })
-    this.questionService.resetAnswered(this.questions).subscribe(
-      response=>{
+    this.questionService.resetAnswered(this.questionsAnswered).subscribe(
+      response => {
         this.resetQuestions = false;
         this.chosenQuestion = null;
       }
     );
+    this.questions = this.route.snapshot.data['questions'];
+
+    this.questionsNotAnswered = [];
+    this.questions.forEach(
+      question =>{
+        this.questionsNotAnswered.push(question);
+      }
+    )
+    this.questionsAnswered = [];
   }
 }
